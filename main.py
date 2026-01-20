80295a8c85033bac24d9749870d80e55843cb6bf
mlritcsedepartment2005-oss <mlritcsedepartment2005@gmail.com>
2026-01-20 16:17:54 +0530

    First mcp server

---
+++ main.py
@@ -0,0 +1,71 @@
+from fastmcp import FastMCP
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(BASE_DIR, "database.db")
+
+mcp = FastMCP(name="Database")
+
+def get_conn():
+    conn = sqlite3.connect(PATH)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            mobileNumber TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    return conn
+
+
+@mcp.tool
+def add_user(username: str, email: str, mobileNumber: str):
+    """Add a new user to the database."""
+    conn =  get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO users (username, email, mobileNumber)
+        VALUES (?, ?, ?)
+    """, (username, email, mobileNumber))
+
+    conn.commit()
+    conn.close()
+
+    return f"User {username} added successfully."
+
+
+@mcp.tool
+def get_user_by_email(email: str):
+    conn =  get_conn()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT id, username, email, created_at, mobileNumber
+        FROM users
+        WHERE email = ?
+    """, (email,))
+
+    user = cursor.fetchone()
+    conn.close()
+
+    if user:
+        return {
+            "id": user[0],
+            "username": user[1],
+            "email": user[2],
+            "created_at": user[3],
+            "mobileNumber": user[4]
+        }
+
+    return f"No user found with email: {email}"
+
+
+if __name__ == "__main__":
+      # ✅ REQUIRED
+    mcp.run(transport="http",host="0.0.0.0",port=8001)  # or "http"
