353fd973fea896f6b5131cbe6a31cec5fa6502f7
mlritcsedepartment2005-oss <mlritcsedepartment2005@gmail.com>
2026-01-20 20:51:24 +0530

    syncio update 2

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ def add(a: int, b: int) -> int:
     return a + b
 
 @mcp.tool
-def add(a: int, b: int) -> int:
+def sub(a: int, b: int) -> int:
     return a - b
 
 @mcp.tool
-def add(a: int, b: int) -> int:
+def mul(a: int, b: int) -> int:
     return a * b
 
 
 @mcp.tool
-def add(a: int, b: int) -> int:
+def mod(a: int, b: int) -> int:
     return a % b
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0" , port=6060)
+    mcp.run(transport="http", host="0.0.0.0" , port=6080)
